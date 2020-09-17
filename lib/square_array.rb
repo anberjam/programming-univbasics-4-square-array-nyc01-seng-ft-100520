0cf6e4417669a1162b69cb2904dcc640e96a1de8
Anber Jamshed <anberjam@yahoo.com>
2020-09-17 21:46:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter=array.length - 1
   while array[counter] do
-    array[counter] = array[counter]
+    array[counter] = array[counter] **2
     counter-=1
   end
   array
